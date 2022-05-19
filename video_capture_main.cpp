854790d874891aab6fe2782e7e83545c4807f637
eunyoung000 <eunyoung@motion2ai.com>
2022-05-19 11:18:09 -0700

    Update the main function

--- video_capture_main.cpp
+++ video_capture_main.cpp
@@ -19,11 +19,15 @@ using namespace zbar;
 namespace {
 
 std::string GetGStreamerPipeline(int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method=0) {
-    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+    return "nvarguscamerasrc aelock=true gainrange=\"7 7\" exposuretimerange=\"5000000 5000000\" !  video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
            std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
            "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
            std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
+
+constexpr int kImageWidth = 1920;
+constexpr int kImageHeight = 1080;
+constexpr int kFramerate = 10;
 
 }  // namespace
 
@@ -34,8 +38,8 @@ int main(int argc, char **argv) {
     if (argc == 2) {
         cam_idx = atoi(argv[1]);
     }
-    VideoCapture capture(GetGStreamerPipeline(/*capture_width=*/4032, /*capture_height=*/3040,
-    /*display_width=*/4032, /*display_height=*/3040, /*frame_rate=*/10), cv::CAP_GSTREAMER);
+    VideoCapture capture(GetGStreamerPipeline(/*capture_width=*/kImageWidth, /*capture_height=*/kImageHeight,
+    /*display_width=*/kImageWidth, /*display_height=*/kImageHeight, /*frame_rate=*/kFramerate), cv::CAP_GSTREAMER);
     if (!capture.isOpened()) {
         cerr << "Could not open camera." << endl;
         exit(EXIT_FAILURE);
@@ -47,10 +51,9 @@ int main(int argc, char **argv) {
     cv::Mat frame;
     capture >> frame;
     VideoWriter writer;
-    int codec = VideoWriter::fourcc('M', 'P', '4', 'V');
-    double fps = 1.0;                          // framerate of the created video stream
-    string filename = "/home/motion2ai/Dev/videos/test_vides/fixed_focus_qr_test.mp4";             // name of the output video file
-    writer.open(filename, codec, fps, frame.size(), true);
+    const int kCodec = VideoWriter::fourcc('M', 'P', '4', 'V');
+    string filename = "../videos/11.mp4";             // name of the output video file
+    writer.open(filename, kCodec, static_cast<double>(kFramerate), frame.size(), true);
     // check if we succeeded
     if (!writer.isOpened()) {
         cerr << "Could not open the output video file for write\n";
@@ -67,16 +70,13 @@ int main(int argc, char **argv) {
         if (frame.empty()) {
             break;
         }
-        cv::Mat frame_resized;
-        resize(frame, frame_resized, Size(1920, 1080));
-        
-        // imshow("Captured frame", frame_resized);
+        imshow("Captured frame", frame);
 
-        auto focused = focuser.Focusing(frame);
-        if (!focused) {
-            continue;
-        }
-        // writer.write(frame);
+        // auto focused = focuser.Focusing(frame);
+        // if (!focused) {
+        //     continue;
+        // }
+        writer.write(frame);
         auto t3 = std::chrono::steady_clock::now();
         cout << "capturing in ms:" << to_string(std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()) << endl;
         cout << "writing in ms:" << to_string(std::chrono::duration_cast<std::chrono::milliseconds>(t3 - t2).count()) << endl;
